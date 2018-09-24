e86a513a64ef4aa5378663cdc0857928263e5ca1
csmark97 <cs.mark97@gmail.com>
2018-09-24 13:43:47 +0200

    New time interval developed

---
+++ src/Places/OpeningInterval.java
@@ -0,0 +1,26 @@
+package Places;
+
+public class OpeningInterval {
+	private boolean isOpen;
+	private MyTime from;
+	private MyTime to;
+	
+	public boolean isOpen() {
+		return isOpen;
+	}
+	public void setOpen(boolean isOpen) {
+		this.isOpen = isOpen;
+	}
+	public MyTime getFrom() {
+		return from;
+	}
+	public void setFrom(MyTime from) {
+		this.from = from;
+	}
+	public MyTime getTo() {
+		return to;
+	}
+	public void setTo(MyTime to) {
+		this.to = to;
+	}
+}
